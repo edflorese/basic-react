e8bd03c584730883229b0a074b3fe687edf8ae71
edflorese <edflorese@uce.edu.ec>
2024-11-23 09:49:22 -0500

    feat: :rocket: Project init

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import  { Profile }  from './Profile'
+
+function App() {
+  return(<section>
+    <h1>Amazing scientists</h1>
+    <Profile />
+    <Profile />
+    <Profile />
+  </section>)
+}
+
+export default App
